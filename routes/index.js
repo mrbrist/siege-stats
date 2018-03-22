ddfc6e8ee286288a1a3cccac504bcc4907aee8c4
mrbristcs <bristol2801@gmail.com>
2018-03-22 21:36:57 +0100

    small update

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(err, req, res, next) {
-  //res.render('index', { title: 'siege-stats' });
-  res.status(err.status || 500);
-  res.render('error');
+  res.render('index', { title: 'siege-stats' });
 });
 
 module.exports = router;
